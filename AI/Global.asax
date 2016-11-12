458d0821e7aaa0041a828e788515ff830b352598
FilthyFrankz <murith.domag@gmail.com>
2016-11-12 20:26:31 +0100

    Initial commit

---
+++ AI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AI.MvcApplication" Language="C#" %>
---
+++ AI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AI.Startup))]
+namespace AI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
